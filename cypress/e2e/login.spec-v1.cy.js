f4bcd776d666fa0d01447cb572fa0909af6f6c2b
luisotacilio <luisotacilio@gmail.com>
2024-10-12 07:42:58 -0300

    Adding BaseUrl

--- cypress/e2e/login.spec-v1.cy.js
+++ cypress/e2e/login.spec-v1.cy.js
@@ -3,8 +3,8 @@ import userData from '../fixtures/users/userData.json'
 
 describe('Orange HRM Tests', () => {
 
-  it.skip('CST001-Login with Sucess', () => {
-      cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    it('CST001-Login with Sucess', () => {
+      cy.visit('/auth/login');
       cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin');
       cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123');
       cy.get('.oxd-button').click();
@@ -12,7 +12,7 @@ describe('Orange HRM Tests', () => {
       cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard');    
     })
     it('CT002-Login with Failure', () => {
-      cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+      cy.visit('/auth/login');
       cy.get("[name='username']").type('Test');
       cy.get("[name='password']").type('Test');
       cy.get('.oxd-button').click();
